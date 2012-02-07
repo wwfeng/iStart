9f3d8d2b8aa1e57ae137aa5d0bc8892154153bc9
wwfeng <produmpling@yahoo.com>
2012-02-07 12:00:29 +0800

    init

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php header("Content-type: text/html; charset=utf-8");?><!DOCTYPE html>
+<!-- vim: set et sw=4 ts=4 sts=4 fdm=marker ff=unix fenc=utf-8: -->
+<html>
+<head>
+<meta charset=utf-8" />
+<title> wwfeng Start Page </title>
+<base target="_blank">
+<style>
+
+</style>
+</head>
+<body >
+<!--[if IE]><div id="fuck-ie" style="display: none"><![endif]-->
+<div id="nav">
+	<div class="nav-wrap">
+        <h1><a href="./">Wwfeng</a></h1>
+		<form id="search" method="get" action="http://www.google.com/search" target="_blank">
+			<input type="text" name="q" />
+			
+			
+			<button type="submit">Google Search</button>
+		</form>
+		
+		<form id="search" method="get" action="http://www.baidu.com/" target="_blank">
+			<input type="text" name="q" />
+			<button type="submit">baidu Search</button>
+		</form>
+
+    </div>
+    
+
+</div>
+<div id="main">
+    <div class="layout grid-m0">
+        <div class="col-main">
+            <div class="main-wrap">
+                <div class="box">
+                    <h2>常用链接</h2>
+                    <dl>
+                        <dt>tools</dt>
+                        <dd>
+					<a href="http://www.bejson.com/">json</a> 
+                        </dd>
+                        <dt>学习交流</dt>
+                        <dd>
+				
+					<a href="http://t.sina.com.cn">新浪微博</a> 
+                        </dd>
+                        <dt>开发文档</dt>
+                        <dd>
+                    <a href="http://jQuery.com">jQuery</a> 
+					<a href="http://code.google.com/appengine/">App Engine</a> 
+					<a href="http://developer.yahoo.com/yui/">YUI</a> 
+					<a href="http://developer.yahoo.com/yql/">YQL</a> 
+					<a href="http://code.google.com/chrome/extensions/">Chrome Extensions</a> 
+					<a href="https://developer.mozilla.org/">Mozilla</a> 
+					<a href="http://php.net">php</a>
+                            
+                        </dd>
+                    </dl>
+                </div>	
+            </div>
+        </div>
+
+        <? /*<div class="col-sub">
+            <div class="box">
+                <div class="desc">
+                    <b class="img" style="background-image: url(assets/avatar.png);"></b>
+                    <a href="">nick</a>
+                    <p>none</p>
+                </div>
+                <form class="post-tweet">
+                    <textarea></textarea>
+                    <button type="submit">Tweet</button>
+                </form>
+            </div>	
+        </div>*/?>
+    </div>
+</div>
+<!--[if IE]></div><center>NO IE !!!</center><![endif]-->
+</body>
+</html> 
